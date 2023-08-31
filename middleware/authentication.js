31f41efa13b8367e32c64de4fae27e74db3c8365
Fernando Siller <fernando@Fernandos-MBP.attlocal.net>
2023-08-31 13:09:34 -0500

    probably did auth correct

---
+++ middleware/authentication.js
@@ -0,0 +1,14 @@
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
